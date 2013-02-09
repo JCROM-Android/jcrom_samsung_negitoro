27701348ee66828c0de4ad1719040a28d0e53fe9
Akihiro Maeda <sola.1980.a@gmail.com>
2013-02-09 21:11:29 +0900

    updated gapps.

--- proprietary/tools/copy-gapps.sh
+++ proprietary/tools/copy-gapps.sh
@@ -4,8 +4,8 @@ SRC_SYSTEM=gapps/system
 DST_SYSTEM=tmp
 
 mkdir gapps
-wget -nc -q http://sola-dolphin-1.net/data/gapps/gapps-jb-4.2-V3-20121127-signed.zip
-unzip gapps-jb-4.2-V3-20121127-signed.zip -d gapps/
+wget -nc -q http://sola-dolphin-1.net/data/gapps/tuna/gapps-jb-20121212-signed.zip
+unzip gapps-jb-20121212-signed.zip -d gapps/
 
 ODEX_LIST=gapps.list
 while read READ_BUF ; do
@@ -14,5 +14,5 @@ cp -a ${SRC_SYSTEM}/app/${READ_BUF}.apk ${DST_SYSTEM}/app/${READ_BUF}.apk
 done < ${ODEX_LIST}
 
 rm -rf gapps
-rm gapps-jb-4.2-V3-20121127-signed.zip
+rm gapps-jb-20121212-signed.zip
 
